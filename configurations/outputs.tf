eb518863a6025654673c56a9551fb5ea767f1517
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 14:07:26 -0400

    Update outputs.tf

--- configurations/outputs.tf
+++ configurations/outputs.tf
@@ -1,6 +1,6 @@
 output "resource-ids" {
   value = <<-EOT
-  Environment ID:   ${confluent_environment.staging.id}
+  Environment ID:   ${data.confluent_environment.staging.id}
   Kafka Cluster ID: ${confluent_kafka_cluster.basic.id}
   EOT
 
