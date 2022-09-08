6b94b07a1cf65a7136b32e7dc7c9ad2c6601cf8e
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 15:54:44 -0400

    Update main.tf

--- configurations/topic-management/main.tf
+++ configurations/topic-management/main.tf
@@ -11,3 +11,14 @@ provider "confluent" {
   cloud_api_key    = var.confluent_cloud_api_key
   cloud_api_secret = var.confluent_cloud_api_secret
 }
+
+data "confluent_environment" "workshop_env" {
+  id = var.confluent_cloud_env_id
+}
+
+data "confluent_kafka_cluster" "workshop_cluster" {
+  id = var.confluent_cloud_cluster_id
+  environment {
+    id = data.confluent_cloud_env_id
+  }
+}
