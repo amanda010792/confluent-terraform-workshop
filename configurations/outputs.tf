8e3f1f89749d32192168f9ca96904a4f8565dee4
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 14:19:30 -0400

    Delete outputs.tf

--- configurations/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "resource-ids" {
-  value = <<-EOT
-  Environment ID:   ${data.confluent_environment.workshop_env.id}
-  Kafka Cluster ID: ${confluent_kafka_cluster.basic.id}
-  EOT
-
-  sensitive = true
-}
