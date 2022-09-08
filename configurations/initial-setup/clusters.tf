9ace469bd37c493087e74024ae29ce3bcffd0e4e
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 15:30:40 -0400

    Update clusters.tf

--- configurations/initial-setup/clusters.tf
+++ configurations/initial-setup/clusters.tf
@@ -5,7 +5,7 @@ data "confluent_environment" "workshop_env" {
 # Update the config to use a cloud provider and region of your choice.
 # https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
 resource "confluent_kafka_cluster" "basic" {
-  display_name = "inventory"
+  display_name = var.confluent_cloud_cluster_name
   availability = "SINGLE_ZONE"
   cloud        = "AWS"
   region       = "us-east-2"
