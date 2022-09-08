4887fba5f718ac44d9cb2a13b403e8fa4e0a1650
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 15:21:58 -0400

    Update cluster-main.tf
    removed provider

--- configurations/initial-setup/cluster-main.tf
+++ configurations/initial-setup/cluster-main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    confluent = {
-      source  = "confluentinc/confluent"
-      version = "1.4.0"
-    }
-  }
-}
-
-provider "confluent" {
-  cloud_api_key    = var.confluent_cloud_api_key
-  cloud_api_secret = var.confluent_cloud_api_secret
-}
-
 data "confluent_environment" "workshop_env" {
   id = var.confluent_cloud_env_id
 }
