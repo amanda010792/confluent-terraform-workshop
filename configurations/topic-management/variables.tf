7278e6dffbdf08b98b4d9c8e17f4163efe93e1af
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 16:31:57 -0400

    Create variables.tf

---
+++ configurations/topic-management/variables.tf
@@ -0,0 +1,22 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+  type        = string
+}
+
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cloud_env_id" {
+  description = "Confluent Cloud Environment ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cloud_cluster_id" {
+  description = "Confluent Cloud Cluster ID (cluster created in initial setup)"
+  type        = string
+  sensitive   = true
+}
