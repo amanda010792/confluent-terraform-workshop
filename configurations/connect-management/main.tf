0cb729fd9399fa03483f196217ad5c1996b20d26
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 17:21:37 -0400

    Update main.tf

--- configurations/connect-management/main.tf
+++ configurations/connect-management/main.tf
@@ -1,1 +1,38 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.4.0"
+    }
+  }
+}
 
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+data "confluent_environment" "workshop_env" {
+  id = var.confluent_cloud_env_id
+}
+
+data "confluent_kafka_cluster" "basic" {
+  id = var.confluent_cloud_cluster_id
+  environment {
+    id = data.confluent_environment.workshop_env.id
+  }
+}
+
+data "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic-cluster.id
+  }
+
+  topic_name    = "orders"
+  rest_endpoint = confluent_kafka_cluster.basic-cluster.rest_endpoint
+
+  credentials {
+    key    = "<Kafka API Key for confluent_kafka_cluster.basic-cluster>"
+    secret = "<Kafka API Secret for confluent_kafka_cluster.basic-cluster>"
+  }
+}
