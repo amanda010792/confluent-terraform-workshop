b214d2f6fe3c80fc5effe5a788bd56d792b4f99a
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 17:57:39 -0400

    Update connectors.tf

--- configurations/connect-management/connectors.tf
+++ configurations/connect-management/connectors.tf
@@ -1,1 +1,28 @@
+resource "confluent_connector" "source" {
+  environment {
+    id = data.confluent_environment.confluent_env.id
+  }
+  kafka_cluster {
+    id = data.confluent_kafka_cluster.basic.id
+  }
 
+  config_sensitive = {}
+
+  config_nonsensitive = {
+    "connector.class"          = "DatagenSource"
+    "name"                     = "DatagenSourceConnector_0"
+    "kafka.auth.mode"          = "SERVICE_ACCOUNT"
+    "kafka.service.account.id" = confluent_service_account.app-connector.id
+    "kafka.topic"              = confluent_kafka_topic.orders.topic_name
+    "output.data.format"       = "JSON"
+    "quickstart"               = "ORDERS"
+    "tasks.max"                = "1"
+  }
+
+  depends_on = [
+    confluent_kafka_acl.app-connector-describe-on-cluster,
+    confluent_kafka_acl.app-connector-write-on-target-topic,
+    confluent_kafka_acl.app-connector-create-on-data-preview-topics,
+    confluent_kafka_acl.app-connector-write-on-data-preview-topics,
+  ]
+}
