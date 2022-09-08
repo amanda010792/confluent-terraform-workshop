1b85c85ab9f458cb1bc942e2298df130d00dda28
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 15:54:34 -0400

    Update topics.tf

--- configurations/topic-management/topics.tf
+++ configurations/topic-management/topics.tf
@@ -1,15 +1,3 @@
-data "confluent_environment" "workshop_env" {
-  id = var.confluent_cloud_env_id
-}
-
-data "confluent_kafka_cluster" "workshop_cluster" {
-  id = var.confluent_cloud_cluster_id
-  environment {
-    id = data.confluent_cloud_env_id
-  }
-}
-
-
 
 resource "confluent_kafka_topic" "orders" {
   kafka_cluster {
