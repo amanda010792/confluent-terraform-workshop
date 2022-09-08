99b390c9490c4e4b1114644417cb9e500f10df2b
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-09-08 16:33:12 -0400

    Update topics.tf

--- configurations/topic-management/topics.tf
+++ configurations/topic-management/topics.tf
@@ -1,7 +1,7 @@
 
 resource "confluent_kafka_topic" "orders" {
   kafka_cluster {
-    id = data.confluent_cloud_cluster_id
+    id = confluent_kafka_cluster.basic.id
   }
   topic_name    = "orders"
   rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
