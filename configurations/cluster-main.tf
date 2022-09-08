4df76d3f38fdc7382a87cbe35c2fd3d967f5f0e5
Amanda Gilbert <agilbert@confluent.io>
2022-09-08 13:56:29 -0400

    moved configs

---
+++ configurations/cluster-main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.4.0"
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+data "confluent_environment" "workshop_env" {
+  id = var.confluent_cloud_env_id
+}
+
+output "workshop_env" {
+  value = data.confluent_environment.workshop_env
+}
+
+# Update the config to use a cloud provider and region of your choice.
+# https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "inventory"
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "us-east-2"
+  basic {}
+  environment {
+    id = data.confluent_environment.workshop_env.id
+  }
+}
